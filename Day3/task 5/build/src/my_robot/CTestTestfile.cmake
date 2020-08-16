fa9c3acdeaecb59daf22cc474213b9f987a1c107
Nada <nadaaymanatia@aucegypt.edu>
2020-08-16 16:09:15 +0200

    Solving errors catkin_make in src workspace

---
+++ Day3/task 5/build/src/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nada/AUC-Robotics-Summer-Camp/Day3/catkin_ws/src/src/my_robot
+# Build directory: /home/nada/AUC-Robotics-Summer-Camp/Day3/catkin_ws/build/src/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Day3/task 5/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(ball_chaser_MESSAGE_FILES "")
+set(ball_chaser_SERVICE_FILES "/home/nada/AUC-Robotics-Summer-Camp/Day3/catkin_ws/src/src/ball_chaser/srv/DriveToTarget.srv")
