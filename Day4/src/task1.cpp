46fe322e331e5e991ada44a02ca7d566b2d327c5
Nada <nadaaymanatia@aucegypt.edu>
2020-08-17 12:17:43 +0200

    Gaussian Equation

--- Day4/src/task1.cpp
+++ Day4/src/task1.cpp
@@ -4,11 +4,10 @@ using namespace std;
 double f(double mu, double sigma2, double x)
 {
 double pie = 22.0 / 7.0;
-double prob = (1/(sigma2*sqrt(2* pie))* exp((pow(((x - mu) / sigma2), 2) * -0.5)));
-
+double prob = 1.0/sqrt(sigma2) * pow(2 * pie, -0.5) * exp(-0.5 * pow( x - mu, 2) * 1.0/sigma2);
 return prob;
 }
 int main() {
 cout << f(10.0, 4.0, 8.0) <<endl;
 return 0;
-}
+};
